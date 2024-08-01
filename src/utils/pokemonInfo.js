7592849e07e766ff98504dba09938176e63b9e3a
Stephanie <cherrygurl5000@yahoo.com>
2024-07-31 22:11:36 -0400

    All basic info including images gathered and defined. Still need id and images for evolution forms.

--- src/utils/pokemonInfo.js
+++ src/utils/pokemonInfo.js
@@ -5,7 +5,10 @@ export async function getPokemon() {
       https://github.com/veekun/pokedex/issues/218#issuecomment-339841781 for removing characters in description text
       https://pokeapi.co/api/v2/region/ for all regions 
       https://pokeapi.co/api/v2/pokemon-form/<pokemon name> or <pokemon id>
+      https://pokeapi.co/api/v2/pokemon/{id or name}/encounters
       https://pokeapi.co/api/v2/evolution-chain/232/ for evolution, but isn't working properly
+      https://img.pokemondb.net/artwork/${pokemonName}.jpg for images
+      https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/025.png for images using 3 digit id
       https://pokeapi.co/api/v2/pokemon?offset=300&limit=1303
       
   */
@@ -20,13 +23,20 @@ export async function getPokemonData(pokemonName) {
     const res1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     const data1 = await res1.json()
 
+    // Find all relevant data and break it into more functional pieces
+    const basic = await basicInfo(data1)
+
+    // Get images based on the id
+    const url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${basic.fullId}.png`
+
     // Get the evolution chain using the Pokemon name
     const res2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`)
     const data2 = await res2.json()
+
     // Get all of the description entries
-    const desc = await data2.flavor_text_entries //.filter(lang => lang.language === 'en')
     // Filter out all descriptions not in english and pass it to the description function
     // to get rid of unnecessary characters and create a single paragraph
+    const desc = await data2.flavor_text_entries
     const descLang = await desc.filter(i => i.language.name === 'en')
     const descText = await descLang.map(i => i=i.flavor_text)
     const fullDesc = await description(descText)
@@ -63,11 +73,47 @@ export async function getPokemonData(pokemonName) {
         // console.log(err)
         evoTo = 'No Final Evolution'
     }
-
+    const evo = await evolution(evoFrom, evoMid, evoTo)
     //console.log( data2, desc, evoFrom, evoMid, evoTo)
-    //console.log(fullDesc)
+    //console.log(basic)
 
-    return {evoFrom, evoTo, evoMid, fullDesc}
+    return {basic, evo, fullDesc}
+}
+
+async function basicInfo(basics) {
+    const { id, height, moves, types, weight } = basics
+    /* 
+        Needed: imperial height (in ft and inches) and weight (in lbs)
+                metric height (in m) and weight (in kg)
+                all types joined by -
+                all moves capitalized and joined by ,
+                3-digit id
+        */
+    // metricHt, metricWt, imperialHt, imperialWt, jointType, jointMoves, fullId
+
+    const metricHt = (height / 10).toString() + " m"
+
+    const imperialHtFt = Math.trunc((height * 3.937007874) / 12).toString() + "\'"
+    const imperialHtIn = Math.round((height * 3.937007874) % 12)
+    const imperialHtInFormatted = (imperialHtIn < 10 ? "0" + imperialHtIn.toString() : imperialHtIn.toString()) + "\""
+    const imperialHt = imperialHtFt + imperialHtInFormatted
+
+    const metricWt = (weight / 10).toFixed(1).toString() + " kg"
+    const imperialWt = (weight / 4.5359237).toFixed(1).toString() + " lbs"
+
+    const allTypes = types.map(type => type.type.name.toLowerCase())
+    const jointType = allTypes.map(index => index.charAt(0).toUpperCase() + index.slice(1)).join('-')
+
+    const allMoves = moves.map(move => move.move.name.toLowerCase())
+    const jointMoves = allMoves.map(index => index.charAt(0).toUpperCase() + index.slice(1)).join(', ')
+
+    let fullId = id.toString()
+    if (fullId.length == 1) fullId = "00" + fullId
+    else if (fullId.length == 2) fullId = "0" + fullId
+
+    // console.log(fullId)
+
+    return {fullId, metricHt, metricWt, imperialHt, imperialWt, jointType, jointMoves}
 }
 
 async function description(arr) {
@@ -102,7 +148,34 @@ async function description(arr) {
 
 }
 
-getPokemonData('flaaffy')
+async function evolution(start, middle, finish) {
+    //console.log(start, middle, finish)
+    let evo = []
+    let noEvo = "Does not evolve"
+    // Add the first evolution to the array
+    // Filter out the additional evolutions if necessary
+    evo.push(start)
+    if (middle === "No Mid Evolution" && finish === "No Final Evolution") {
+        evo.push(noEvo)
+    }
+    else if (middle === "No Mid Evolution") {
+        evo.push(finish)
+    }
+    else if (finish === "No Final Evolution") {
+        evo.push(middle)
+    }
+    else {
+        evo.push(middle)
+        evo.push(finish)
+    }
+
+    console.log(evo)
+    return evo
+}
+
+getPokemonData('pikachu')
+// getPokemonData('bulbasaur')
+//  getPokemonData('electrode')
 // getPokemonData('mareep')
 // getPokemonData('shinx')
 // getPokemonData('luxio')
